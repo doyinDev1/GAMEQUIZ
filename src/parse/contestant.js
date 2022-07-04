38b140e75087e65ef7de4fea5495831320ed049e
doyinDev1 <adedoyin.oyebanji@9ijakids.com>
2022-07-04 12:08:32 +0100

    last last

--- src/parse/contestant.js
+++ src/parse/contestant.js
@@ -1,45 +1,45 @@
 import Parse from 'parse';
 import { useQuery } from 'react-query';
 
-export const fetchUserDetails = async (username) => {
-	try {
-		const params = { username: username };
-		const userSummary = await Parse.Cloud.run('userDetails', params);
-		const summary = await JSON.parse(JSON.stringify(userSummary));
+// export const fetchUserDetails = async (username) => {
+// 	try {
+// 		const params = { username: username };
+// 		const userSummary = await Parse.Cloud.run('userDetails', params);
+// 		const summary = await JSON.parse(JSON.stringify(userSummary));
 
-		const challengeLog = await Parse.Cloud.run('getUserChallengeLog', params);
-		const logsData = await JSON.parse(JSON.stringify(challengeLog));
+// 		const challengeLog = await Parse.Cloud.run('getUserChallengeLog', params);
+// 		const logsData = await JSON.parse(JSON.stringify(challengeLog));
 
-		// const user = await Parse.Cloud.run('fetchAllCandidate', params);
-		// console.log(user);
-		return {
-			userSummary: summary[0],
-			challenge_log: logsData,
-		};
-	} catch (error) {
-		throw error;
-	}
-};
+// 		// const user = await Parse.Cloud.run('fetchAllCandidate', params);
+// 		// console.log(user);
+// 		return {
+// 			userSummary: summary[0],
+// 			challenge_log: logsData,
+// 		};
+// 	} catch (error) {
+// 		throw error;
+// 	}
+// };
 
-export const useFetchUserInfo = () => {
-	const userInfo = JSON.parse(sessionStorage.getItem('contestant'));
+// export const useFetchUserInfo = () => {
+// 	const userInfo = JSON.parse(sessionStorage.getItem('contestant'));
 
-	const fetchDetails = async () => {
-		try {
-			const params = { username: userInfo.username };
-			const userDetails = await Parse.Cloud.run('fetchUserDetails', params);
-			return userDetails;
-		} catch (error) {
-			return error;
-		}
-	};
+// 	const fetchDetails = async () => {
+// 		try {
+// 			const params = { username: userInfo.username };
+// 			const userDetails = await Parse.Cloud.run('fetchUserDetails', params);
+// 			return userDetails;
+// 		} catch (error) {
+// 			return error;
+// 		}
+// 	};
 
-	const { status, data, isFetching } = useQuery(['userDetails', userInfo], () => fetchDetails(), {
-		keepPreviousData: true,
-		refetchOnWindowFocus: true,
-	});
-	return { status, data, isFetching };
-};
+// 	const { status, data, isFetching } = useQuery(['userDetails', userInfo], () => fetchDetails(), {
+// 		keepPreviousData: true,
+// 		refetchOnWindowFocus: true,
+// 	});
+// 	return { status, data, isFetching };
+// };
 
 
 export const doLogin = async (value) => {
@@ -84,45 +84,45 @@ export const getQuizList = async (value) => {
 };
 
 
-export const getChallenges = async (username) => {
-	try {
-		const params = { username: username };
-		const data = await Parse.Cloud.run('getChallenges', params);
-		const allChallenges = await JSON.parse(JSON.stringify(data));
-		return allChallenges;
-	} catch (error) {
-		throw error;
-	}
-};
+// export const getChallenges = async (username) => {
+// 	try {
+// 		const params = { username: username };
+// 		const data = await Parse.Cloud.run('getChallenges', params);
+// 		const allChallenges = await JSON.parse(JSON.stringify(data));
+// 		return allChallenges;
+// 	} catch (error) {
+// 		throw error;
+// 	}
+// };
 
-export const getToken = async (employeeid) => {
-	try {
-		const params = { employeeid: employeeid };
-		const data = await Parse.Cloud.run('generateToken', params);
-		// const allChallenges = await JSON.parse(JSON.stringify(data));
-		return data;
-	} catch (error) {
-		throw error;
-	}
-};
+// export const getToken = async (employeeid) => {
+// 	try {
+// 		const params = { employeeid: employeeid };
+// 		const data = await Parse.Cloud.run('generateToken', params);
+// 		// const allChallenges = await JSON.parse(JSON.stringify(data));
+// 		return data;
+// 	} catch (error) {
+// 		throw error;
+// 	}
+// };
 
-export const saveChallengeLog = async (data) => {
-	try {
-		const log = await Parse.Cloud.run('saveChallengeLog', data);
-		const challengeData = await JSON.parse(JSON.stringify(log));
-		return challengeData;
-	} catch (error) {
-		throw error;
-	}
-};
+// export const saveChallengeLog = async (data) => {
+// 	try {
+// 		const log = await Parse.Cloud.run('saveChallengeLog', data);
+// 		const challengeData = await JSON.parse(JSON.stringify(log));
+// 		return challengeData;
+// 	} catch (error) {
+// 		throw error;
+// 	}
+// };
 
-export const getUserBadges = async (username) => {
-	try {
-		const params = { username: username };
-		const data = await Parse.Cloud.run('getUserBadges', params);
-		const allBadges = await JSON.parse(JSON.stringify(data));
-		return allBadges;
-	} catch (error) {
-		throw error;
-	}
-};
+// export const getUserBadges = async (username) => {
+// 	try {
+// 		const params = { username: username };
+// 		const data = await Parse.Cloud.run('getUserBadges', params);
+// 		const allBadges = await JSON.parse(JSON.stringify(data));
+// 		return allBadges;
+// 	} catch (error) {
+// 		throw error;
+// 	}
+// };
